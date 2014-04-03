ec5f973388605159c3d5e473934e7c5146aced80
Yahong Ding <yahongding1028@gmail.com>
2014-04-03 09:48:16 -0500

    modified the css and master page and added some codes at master page

--- MasterPage.master.vb
+++ MasterPage.master.vb
@@ -1,5 +1,11 @@
 ﻿
 Partial Class MasterPage
     Inherits System.Web.UI.MasterPage
+
+    Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+        'Display the date in the #menubar div
+        lbl_date.Text = DateTime.Now.ToLongDateString
+
+    End Sub
 End Class
 
